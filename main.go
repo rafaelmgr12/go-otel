7daba9b7a6312802a3f87d3e1806f927f3cb5d22
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-11-25 17:45:27 -0300

    Add Prometheus exporter and HTTP handler

--- main.go
+++ main.go
@@ -5,27 +5,20 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/metric/global"
-	"go.opentelemetry.io/otel/metric/instrument"
 )
 
 func main() {
-	exporter := prometheus.New()
-	global.SetMeterProvider(exporter.MeterProvider())
+	exporter, err := prometheus.New()
+	if err != nil {
+		log.Fatalf("failed to initialize prometheus exporter: %v", err)
+	}
 
-	meter := global.Meter("test-meter")
-
-	requestCounter, _ := meter.SyncInt64().Counter(
-		"api_requests_total",
-		instrument.WithDescription("Total number of requests"),
-	)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		requestCounter.Add(r.Context(), 1)
-		w.Write([]byte("Hello, World!"))
-	})
-
+	// Cria um servidor HTTP para expor as métricas
+	http.HandleFunc("/", handler)
 	http.HandleFunc("/metrics", exporter.ServeHTTP)
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Sua lógica de manipulação de requisições aqui
 }
