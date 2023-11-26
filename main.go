e28a8af896dbad6118badb6f2d759a4a9e85e389
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-11-26 09:56:08 -0300

    Refactor rolldice function to use random number generator

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
+func rolldice(w http.ResponseWriter, r *http.Request) {
+	roll := 1 + rand.Intn(6)
+
+	resp := strconv.Itoa(roll) + "\n"
+	if _, err := io.WriteString(w, resp); err != nil {
+		log.Printf("Write failed: %v\n", err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/rolldice", rolldice)
 
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Simple Server")
-}
-
-func statusHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status: OK")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
